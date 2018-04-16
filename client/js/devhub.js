77a4ae8fed7b0e875d6d1e8553218ea65283b590
Dan Cobb <cobbdb@gmail.com>
2018-04-16 02:50:21 -0700

    .

--- client/js/devhub.js
+++ client/js/devhub.js
@@ -3,9 +3,6 @@ var filter = require('./filter.js'),
     buildTable = require('./build-table-body.js');
 
 $.getJSON('http://www.dcobb.media/cmg/devhub/api/ping.php?key=3.1415', function (res) {
-    console.log(res);
-}).fail(function (jqXHR) {
-    console.error(jqXHR.status, jqXHR.statusText);
     //var res = {"records":[{"user":"dcobb","path":"test\/path1234.js","time":1523656809},{"user":"mheydari","path":"test\/path1.js","time":1523657365},{"user":"mheydari","path":"test\/path10.js","time":1523657356},{"user":"mheydari","path":"test\/path189.js","time":1523657489},{"user":"mheydari","path":"test\/path18989.js","time":1523659853}]};
 
     res.records.forEach(function (record) {
@@ -20,4 +17,6 @@ $.getJSON('http://www.dcobb.media/cmg/devhub/api/ping.php?key=3.1415', function 
         var filtered = filter(buildFilter(), res.records);
         buildTable(filtered);
     });
+}).fail(function (jqXHR) {
+    console.error(jqXHR.status, jqXHR.statusText);
 });
